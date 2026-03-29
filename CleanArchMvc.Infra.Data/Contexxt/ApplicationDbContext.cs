e9aa07aa8595f79544fcc7a15ae6ef72d7867a90
tonSan+os <toncsj19@gmail.com>
2026-03-29 18:33:08 -0300

    Add project files.

---
+++ CleanArchMvc.Infra.Data/Contexxt/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.Contexxt
+{
+    public class ApplicationDbContext : DbContext // padrao repository e energy of work
+    {
+        //envia nos parametros o provedor do banco de dados e a string de conexao
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base (options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+        }
+    }
+}
